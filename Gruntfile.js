fa6322c2c323ab01c1b050cf6111a6030086ed73
Josh Kieschnick <jjkiesch@gmail.com>
2014-10-26 01:53:31 -0500

    Add bootstrap to theme.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,10 @@ module.exports = function(grunt) {
 
   // Theme configuration
   var config = {
-    contentDir: '../wordpress/content/',
+    contentDir: '../wordpress/content',
     themeName: 'prepress',
-    theme: 'theme/',
-    dist: 'dist/'
+    theme: 'theme',
+    dist: 'dist'
   };
 
   // Configure grunt tasks
@@ -27,10 +27,19 @@ module.exports = function(grunt) {
     },
 
     compass: {
+      options: {
+        sassDir: '<%= yeoman.theme %>/scss',
+        cssDir: '<%= yeoman.theme %>/css',
+        generatedImagesDir: '<%= yeoman.theme %>/images/generated',
+        imagesDir: '<%= yeoman.theme %>/images',
+        javascriptsDir: '<%= yeoman.theme %>/js',
+        fontsDir: '<%= yeoman.theme %>/fonts',
+        importPath: './bower_components',
+        relativeAssets: true
+      },
       dev: {
         options: {
-          sassDir: '<%= yeoman.theme %>/scss',
-          cssDir: '<%= yeoman.theme %>/css'
+          debugInfo: true
         }
       }
     },
@@ -50,5 +59,17 @@ module.exports = function(grunt) {
         ]
       }
     }
+  });
+
+  grunt.registerTask('serve', 'Compile and watch theme assets', function(target) {
+    if (target === 'dist') {
+      // Handle dist build.
+    }
+
+    grunt.task.run([
+      'symlink:dev',
+      'compass:dev',
+      'watch'
+    ]);
   });
 };
